76781a3310ba2f8b68dc952ff5d0019f6f973952
IPospelova <iri_shka_s@mail.ru>
2021-03-21 14:30:28 +0300

    add - enter point

---
+++ src/components/Header.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="text--14 text--center mt-20">HELOOOOOOOOOOOOOO</h1>
+</template>
---
+++ src/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <page-header />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+
+export default {
+  name: 'Home',
+  components: {
+    'page-header': Header
+  }
+}
+</script>
